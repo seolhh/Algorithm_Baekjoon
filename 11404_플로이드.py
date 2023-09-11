87b53e2ceded4106c8eb20550baea1c2c4afc02f
seolhyehyeon <seolhyehyeon@gmail.com>
2023-09-12 07:41:05 +0900

    0912

---
+++ 11404_플로이드.py
@@ -0,0 +1,15 @@
+n= int(input())
+m= int(input())
+
+bus_line=[]
+for i in range(m+2):
+    bus_line.append(list(map(int,input().split())))
+    # print(bus_line)
+
+for i in range(m):
+    for j in range(2):
+        for k in range()
+        print(bus_line[i][j])
+
+
+
--- 1463_1로만들기.py
+++ 1463_1로만들기.py
@@ -7,7 +7,7 @@ for i in range(2,N+1):
     dp[i] = dp[i-1]+1    # +1하는 경우는 무조건 가능하니까
     # print(dp[i])
     if i%2 ==0:
-        dp[i] = min(dp[i] , dp[i//2]+1)   ## 왜 +1을 해주는거지?
+        dp[i] = min(dp[i] , dp[i//2]+1)   # dp[i]는 우선 dp[i-1]+1의 값은 항상 가능한데 %2가 가능한 경우엔 두 값음 중 최소값을 넣
     # print(dp[i//2]+1)
     if i%3 ==0:
         dp[i] = min(dp[i] , dp[i//3] +1)
